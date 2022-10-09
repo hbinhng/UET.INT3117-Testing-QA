2e83a501f9a03bd24c572c06ba2268fa07a02d25
beast462 <binhmattroi@gmail.com>
2022-10-09 13:17:20 +0700

    (week4) update jsdoc, add item type guard

--- weeks/week4/src/maths.ts
+++ weeks/week4/src/maths.ts
@@ -4,9 +4,9 @@ export class Maths {
    * @param {number[]} input array of number
    * @param {number} [start=0] start of range to be calculated (will be floored)
    * @param {number} [length=Number.MAX_SAFE_INTEGER] length of range to be calculated (will be floored)
-   * @throws {Error} "unsafe argument" if input contains number out of range
-   * [{@link Number.MIN_SAFE_INTEGER}, {@link Number.MAX_SAFE_INTEGER}] or start is out of range
-   * [0, {@link Number.MAX_SAFE_INTEGER}] or length is out of range [1, {@link Number.MAX_SAFE_INTEGER}]
+   * @throws {Error} "unsafe argument" if input contains item out of range
+   * [{@link Number.MIN_SAFE_INTEGER}, {@link Number.MAX_SAFE_INTEGER}] or not even a number, start is out of range
+   * [0, {@link Number.MAX_SAFE_INTEGER}], length is out of range [1, {@link Number.MAX_SAFE_INTEGER}]
    */
   public static average(
     input: number[],
@@ -24,6 +24,7 @@ export class Maths {
     const range = input.slice(_start, _start + _length);
 
     const average = range.reduce((accumulator, value) => {
+      if (typeof value !== 'number') throw new Error('unsafe argument');
       if (value < Number.MIN_SAFE_INTEGER || value > Number.MAX_SAFE_INTEGER)
         throw new Error('unsafe argument');
 
